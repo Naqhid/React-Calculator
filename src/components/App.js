5a05379eede13db7466b5ba16eb40ccae18afcd4
Mohammed Naqhid <mnaqhid@gmail.com>
2021-07-06 16:11:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
       <>
         <div className="App">
           <div className="content">
-            <h1 className="title-app">Welcome To My Calculator</h1>
+            <h1 className="title-app">Naqhid Calculator</h1>
             <Display total={total} operation={operation} next={next} />
             <ButtonPanel clickHandler={this.handleClick} />
           </div>
